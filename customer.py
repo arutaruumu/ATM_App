05bb75946b9efc656e1bbee61c6a0cee92127dfd
irongolembug <80472103+irongolembug@users.noreply.github.com>
2021-03-11 23:07:38 +0700

    Create customer.py

---
+++ customer.py
@@ -0,0 +1,15 @@
+class Customer:
+  def init(self,id,custPin = 1234, custBalance = 10000):
+    self.id = id
+    self.pin = custPin
+    self.balance = custBAlance
+  def checkID(self):
+    return self.id
+  def checkPin(self):
+    return self.pin
+  def checkBalance(self):
+    return self.balance
+  def withdrawBalance(self):
+    self.balance = nominal
+  def depositBalance(self):
+    self.balance = nominal
